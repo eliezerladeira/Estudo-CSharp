2447b283113018c58ddc0fc25eb9ed5ba19e1d38
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-27 17:06:33 -0300

    Operações Matemáticas
    Utilizando operadores matemáticos, exibindo mensagens na tela em modo texto e realizando a leitura de uma tecla.

---
+++ Operacoes/Operacoes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que realiza as operações matemáticas básicas");
+            Console.WriteLine("5 + 10 = ");
+            Console.WriteLine(5 + 10);
+            Console.ReadKey();
+        }
+    }
+}
