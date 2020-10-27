667141f7aeb2616d0b0d7dc09623b3ec16185b67
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-27 19:28:28 -0300

    Soma de Dois Números (double)
    Variáveis do tipo Double

--- SomaDoisNumeros/SomaDoisNumeros/Program.cs
+++ SomaDoisNumeros/SomaDoisNumeros/Program.cs
@@ -10,12 +10,13 @@ namespace SomaDoisNumeros
     {
         static void Main(string[] args)
         {
+            // variáveis do tipo String
             int valor1 = 0;
             int valor2 = 0;
             int total = 0;
             String entrada = "";
 
-            Console.WriteLine("Programa que realiza a soma de dois números");
+            Console.WriteLine("Programa que realiza a soma de dois números - int");
 
             Console.Write("Informe o 1º valor: ");
             entrada = Console.ReadLine();
@@ -27,6 +28,23 @@ namespace SomaDoisNumeros
 
             total = valor1 + valor2;
             Console.WriteLine(valor1 + " + " + valor2 + " = " + total);
+            Console.ReadKey();
+
+            // variáveis do tipo double
+            double valor3 = 0;
+            double valor4 = 0;
+            double total1 = 0;
+
+            Console.WriteLine("Programa que realiza a soma de dois números - double");
+
+            Console.Write("Informe o 1º valor: ");
+            valor3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe o 2º valor: ");
+            valor4 = Convert.ToDouble(Console.ReadLine());
+
+            total1 = valor3 + valor4;
+            Console.WriteLine(valor3 + " + " + valor4 + " = " + total1);
             Console.ReadKey();
         }
     }
