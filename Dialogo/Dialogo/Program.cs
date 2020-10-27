9cbd18ed7f18812b2c70a1479aef943c0eb70a84
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-27 18:55:20 -0300

    Diálogo - String
    Variável do tipo String

---
+++ Dialogo/Dialogo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dialogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String usuarioNome = "";
+            Console.WriteLine("Diálogo entre o PC e o usuário");
+            Console.Write("Qual o seu nome? ");
+            usuarioNome = Console.ReadLine();
+            Console.WriteLine("Olá " + usuarioNome);
+            Console.ReadKey();
+        }
+    }
+}
