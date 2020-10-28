d083ab46fb21be7f0c4f5b427e8c16bb5733d507
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-27 22:47:27 -0300

    Lados de um triângulo
    Programa que verifica se três valores informados pelo usuário em modo console representam os lados de um triângulo.

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Desenvolva um programa para verificar se três valores informados representam os
+             * lados de um triângulo. (O comprimento de cada lado do triângulo é menor do que a
+             * soma dos comprimentos dos outros dois lados)
+             * */
+            int a = 0, b = 0, c = 0;
+
+            String msg = "Os valores informados não representam os lados de um triângulo.";
+            Console.WriteLine("Verifica se três valores informados representam os lados de um triângulo");
+
+            Console.Write("Informe o valor do lado 1 do triângulo: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o valor do lado 2 do triângulo: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o valor do lado 3 do triângulo: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a < b + c)
+            {
+                if (b < a + c)
+                {
+                    if (c < b + a)
+                    {
+                        msg="Os valores informados representam os lados de um triângulo";
+                    }
+                }
+            }
+
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
