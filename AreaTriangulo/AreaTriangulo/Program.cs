04c29afea47726f3b88fe041a4d8f0ac7a16bee5
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-27 22:05:59 -0300

    Área de um Triângulo
    Programa em modo console que calcula a área de um triângulo.

---
+++ AreaTriangulo/AreaTriangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // calcular a área de um triângulo
+            double baseTri = 0, alturaTri = 0;
+
+            Console.WriteLine("Calcula a área de um triângulo");
+
+            Console.WriteLine("Informe a base do triângulo: ");
+            baseTri = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do triângulo: ");
+            alturaTri = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Área = " + ((baseTri * alturaTri) / 2));
+            Console.ReadKey();
+        }
+    }
+}
