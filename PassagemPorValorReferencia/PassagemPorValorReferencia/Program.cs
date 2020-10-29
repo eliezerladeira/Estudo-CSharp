66cf14e466565220e6cb73aba472f7310531342f
eliezerladeira <eliezerladeira@yahoo.com.br>
2020-10-28 22:44:53 -0300

    Passagem de parâmetros por valor e por referência

---
+++ PassagemPorValorReferencia/PassagemPorValorReferencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassagemPorValorReferencia
+{
+    class Program
+    {
+        static void passagemPorValor(int a)
+        {
+            a = 10;
+        }
+
+        static void passagemPorReferencia(ref int a)
+        {
+            a = 10;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 5;
+            passagemPorValor(n);
+            Console.WriteLine("Passagem de parâmetros por valor: " + n);
+
+            passagemPorReferencia(ref n);
+            Console.WriteLine("Passagem de parâmetros por referência: " + n);
+            Console.ReadKey();
+        }
+    }
+}
