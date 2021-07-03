32b51ef76039d0922f683ff38366c908cd7fd014
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-07-03 00:11:22 -0300

    Comparison
    Usando estrutura IF e operadores

---
+++ Como Programar/Comparison/Comparison/Program.cs
@@ -0,0 +1,41 @@
+﻿// Usando instruções if, operadores relacionais e operadores de igualdade
+
+using System;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1,     // primeiro número a comparar
+                number2;     // segundo número a comparar
+
+            // lê o primeiro número do usuário
+            Console.Write("Please enter first integer: ");
+            number1 = Int32.Parse(Console.ReadLine());
+
+            // lê o segundo número do usuário
+            Console.Write("\nPlease enter second integer: ");
+            number2 = Int32.Parse(Console.ReadLine());
+
+            if (number1 == number2)
+                Console.WriteLine(number1 + " == " + number2);
+
+            if (number1 != number2)
+                Console.WriteLine(number1 + " != " + number2);
+
+            if (number1 < number2)
+                Console.WriteLine(number1 + " < " + number2);
+
+            if (number1 > number2)
+                Console.WriteLine(number1 + " > " + number2);
+
+            if (number1 <= number2)
+                Console.WriteLine(number1 + " <= " + number2);
+
+            if (number1 >= number2)
+                Console.WriteLine(number1 + " >= " + number2);
+        } // fim do método Main
+    } // fim da classe Comparison
+}
