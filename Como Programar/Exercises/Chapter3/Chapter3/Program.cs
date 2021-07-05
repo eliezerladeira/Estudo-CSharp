da3f752c493e419ce7d7942a9a77595b8f83a616
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-07-05 16:55:36 -0300

    Operadores de Atribuição

---
+++ Como Programar/Exercises/Chapter3/Chapter3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exiba a mensagem "enter two number" usando a classe MessageBox
+            MessageBox.Show("Enter two numbers");
+
+            // atribua o produto das variaveis b e c à variável a
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            a = b * c;
+
+            // diga que um programa efetua um exemplo de cálculo de folha de pagamento (isto é, use texto que ajude a documentar um programa
+        }
+    }
+}
