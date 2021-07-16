b16f71418819e2e76e7a13e2e8cb63e73dcc01f5
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-07-16 10:11:24 -0300

    Include Hotel

---
+++ Hotel/Sistema Inicial/SistemaHotel/SistemaHotel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaHotel
+{
+    static class Program
+    {
+        //DECLARAR AS VARIAVEIS GLOBAIS DO SISTEMA
+        public static string nomeUsuario;
+        public static string cargoUsuario;
+
+        public static string chamadaProdutos;
+
+        public static string nomeProduto;
+        public static string estoqueProduto;
+        public static string valorProduto;
+        public static string idProduto;
+
+        public static string idVenda;
+
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
